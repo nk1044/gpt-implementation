ed34d48c110849459a1a05cb473ecfd121a28284
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2026-07-15 21:04:46 +0530

    add multi head attention

--- attention_head.py
+++ attention_head.py
@@ -23,3 +23,13 @@ class Head(nn.Module):
         out = wei @ v
         return out
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(N_EMBEDDINGS, N_EMBEDDINGS)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
